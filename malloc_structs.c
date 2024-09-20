e25c2e60dd0b82cfdba977c783f6d6db481c80f5
Luis Victor <168865256+lluisvl@users.noreply.github.com>
2024-09-20 08:43:30 -0600

    Add files via upload

---
+++ malloc_structs.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+/* Ejercicios Malloc / Free 
+  /* Ahora con estructuras */
+
+
+  /* 4, crea un pollito con ID 1*/
+  
+
+  /* 4.1, crea 3 pollitos mas! no pierdas la referencia a ellos, ponles el ID que queiras */
+  
+
+  /* 4.2 crea un arreglo de para guardar estos 4 pollitos existentes ...
+     De que tipo de dato sera? ... tipo apuntador a pollito  
+  */
+  
+
+  /* 4.3 imprime los pollitos usando el arreglo */
+  
+
+  /*ya no quiero los pollitos, liberalos en la granja (TODOS)*/
+  return 0;
+} 
